d21523b589716fc573a6c728aa4217ce34dc6658
aman <aman2467@gmail.com>
2015-09-10 16:55:35 +0530

    Added file header to screen_saver.c and removed event QUIT

--- screen_saver/screen_saver.c
+++ screen_saver/screen_saver.c
@@ -1,49 +1,58 @@
+/* ==========================================================================
+ * @file    : screen_saver.c
+ *
+ * @description : This file contains main program.
+ *
+ * @author  : Aman Kumar (2015)
+ *
+ * @copyright   : The code contained herein is licensed under the GNU General
+ *		Public License. You may obtain a copy of the GNU General
+ *		Public License Version 2 or later at the following locations:
+ *              http://www.opensource.org/licenses/gpl-license.html
+ *              http://www.gnu.org/copyleft/gpl.html
+ * ========================================================================*/
+
 #include <SDL/SDL.h>
+
+#define ever ;;
 
 int main()
 {
 	SDL_Surface* screen;
 	SDL_Event event;
 	char *key;
-	int color1 = 0, color2 = 0;
-	int kill = 0, reached = 0;
+	int colorG = 0, colorB = 0;
+	int reached = 0;
 
 	SDL_Init(SDL_INIT_VIDEO);
 	screen = SDL_SetVideoMode(1366, 768, 32,
 				  SDL_SWSURFACE | SDL_FULLSCREEN);
 
-	while(1) {
+	for(ever) {
 		SDL_FillRect(screen, NULL,
-			     SDL_MapRGB(screen->format, 255, color1,
-					color2));
+			     SDL_MapRGB(screen->format, 255, colorG,
+					colorB));
 		usleep(10000);
-		key = SDL_GetKeyState(NULL);
 		SDL_Flip(screen);
 		if(!reached) {
-			if((color1 < 255) && (color2 == 0)) {
-				color1++;
-			} else if((color1 == 255) && (color2 < 255)) {
-				color2++;
-			} else if((color1 == 255) && (color2 == 255)) {
+			if((colorG < 255) && (colorB == 0)) {
+				colorG++;
+			} else if((colorG == 255) && (colorB < 255)) {
+				colorB++;
+			} else if((colorG == 255) && (colorB == 255)) {
 				reached = 1;
 			}
 		} else if(reached) {
-			if(color1 == 255 && color2 > 0) {
-				color2--;
-			} else if(color1 > 0 && color2 == 0) {
-				color1--;
-			} else if(color1 == 0 && color2 == 0) {
+			if(colorG == 255 && colorB > 0) {
+				colorB--;
+			} else if(colorG > 0 && colorB == 0) {
+				colorG--;
+			} else if(colorG == 0 && colorB == 0) {
 				reached = 0;
 			}
 		}
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-				case SDL_QUIT:
-					kill = 1;
-					break;
-			}
-		}
-		if(kill) break;
+		SDL_PumpEvents();
+		key = SDL_GetKeyState(NULL);
 		if(key[SDLK_ESCAPE]) break;
 	}
 	SDL_Quit();
